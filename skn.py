7f67b0f8867fddbb1b4b8f7a1baf6abe0804b22b
ralliso10 <58442580+ralliso10@users.noreply.github.com>
2020-04-24 13:54:26 -0400

    Update skn.py

--- skn.py
+++ skn.py
@@ -109,3 +109,35 @@ print(rscore)
 f1score=metrics.f1_score(y_test.ravel(), y_pred.ravel(), average=None)
 print("F1 Score:")
 print(f1score)
+#learning curve
+def plot_learning_curve(estimator, title, X, y, ylim=None, cv=None, n_jobs=None, train_sizes=np.linspace(.1, 1.0, 5)):
+    plt.figure()
+    plt.title(title)
+    if ylim is not None:
+        plt.ylim(*ylim)
+    plt.xlabel("Training examples")
+    plt.ylabel("Score")
+    train_sizes, train_scores, test_scores = learning_curve(estimator, X, y, cv=cv, n_jobs=n_jobs, train_sizes=train_sizes)
+    train_scores_mean = np.mean(train_scores, axis=1)
+    train_scores_std = np.std(train_scores, axis=1)
+    test_scores_mean = np.mean(test_scores, axis=1)
+    test_scores_std = np.std(test_scores, axis=1)
+    plt.grid()
+
+    plt.fill_between(train_sizes, train_scores_mean - train_scores_std, train_scores_mean + train_scores_std, alpha=0.1, color="r")
+    plt.fill_between(train_sizes, test_scores_mean - test_scores_std, test_scores_mean + test_scores_std, alpha=0.1, color="g")
+    plt.plot(train_sizes, train_scores_mean, 'o-', color="r", label="Training score")
+    plt.plot(train_sizes, test_scores_mean, 'o-', color="g", label="Cross-validation score")
+    plt.legend(loc="best")
+    directory = 'graph_pictures'
+    
+    if not os.path.exists('graph_pictures'):
+        os.makedirs('graph_pictures')
+    
+    directory += '/skn_learning_curve.png'
+    plt.savefig(directory)
+
+title = "Learning Curves (SVM, RBF kernel, $\gamma=0.001$)"
+# SVC is more expensive so we do a lower number of CV iterations:
+cv = ShuffleSplit(n_splits=10, test_size=0.2, random_state=0)
+plot_learning_curve(cnb, title, X, y, (0.7, 1.01), cv=cv, n_jobs=4)
